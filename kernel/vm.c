7f4dbad6feeaf7b17dd60fdb83d4407ebc85c968
Panagiotis Galopoulos <gl.panagiotis@gmail.com>
2021-02-06 23:17:50 +0200

    Page table lab ex 1, "print a page table"

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint_helper(pagetable_t pagetable, int level)
+{
+  char prefixes[][10] = {
+    "..",
+    ".. ..",
+    ".. .. ..",
+  };
+  for (int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if (!(pte & PTE_V)){
+      continue;
+    }
+    printf(prefixes[level]);
+    printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    if ((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      uint64 child = PTE2PA(pte);
+      vmprint_helper((pagetable_t)child, level + 1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 0);
+}
